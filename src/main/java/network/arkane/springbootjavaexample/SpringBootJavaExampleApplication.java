fac2e292fbf3324d7f78cce44cd5386f5c5c0cde
Quinten De Swaef <quintendeswaef@gmail.com>
2018-12-18 16:47:25 +0100

    Initial version

---
+++ src/main/java/network/arkane/springbootjavaexample/SpringBootJavaExampleApplication.java
@@ -0,0 +1,14 @@
+package network.arkane.springbootjavaexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJavaExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJavaExampleApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/network/arkane/springbootjavaexample/SpringBootJavaExampleApplicationTests.java
@@ -0,0 +1,17 @@
+package network.arkane.springbootjavaexample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootJavaExampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
