55396eb8a814c7b1ef793bebfc381655072a34ba
Quinten De Swaef <quintendeswaef@gmail.com>
2018-12-18 17:07:44 +0100

    starting documentation

--- src/test/java/network/arkane/springbootjavaexample/ExampleApplicationTest.java
+++ src/test/java/network/arkane/springbootjavaexample/ExampleApplicationTest.java
@@ -7,11 +7,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringBootJavaExampleApplicationTests {
+public class ExampleApplicationTest {
 
 	@Test
 	public void contextLoads() {
 	}
-
 }
 
